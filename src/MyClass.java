9a9e2b356526b3b29649f5de0a0481506ef0ade4
gumburtu <aslanseval1986@gmail.com>
2025-02-07 17:47:17 +0000

    V2

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,6 @@ public class MyClass {
     public static void main(String[] args) {
         //degisiklik-1
         System.out.println("Herkese selamlar");
-
+        //advance github dersine hosgeldiniz
     }
 }
