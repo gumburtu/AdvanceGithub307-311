33edb0b5207be9083433ed3d8a0b54e1c89e1194
gumburtu <aslanseval1986@gmail.com>
2025-02-07 19:00:40 +0000

    V10

--- src/Book.java
+++ src/Book.java
@@ -2,5 +2,7 @@ public class Book {
     public static void main(String[] args) {
         //burada da bir seyler yazdik
         //seval branci icin degisiklik
+        //degisiklik-3
+        System.out.println("sevalaslan");
     }
 }
